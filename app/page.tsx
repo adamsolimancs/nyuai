548ed5e67e9bb3df703f05f79cec6089e7e67a00
Adam Soliman <aes10130@nyu.edu>
2025-06-28 19:23:58 -0400

    Coming soon website displayed for initial deployment

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1 id={styles.title} >AI @ NYU</h1>
       <main className={styles.main}>
-        AI @ NYU
-        Coming soon.
+        Coming soon
       </main>
       <footer className={styles.footer}>
-        Contact
+        
       </footer>
     </div>
   );
